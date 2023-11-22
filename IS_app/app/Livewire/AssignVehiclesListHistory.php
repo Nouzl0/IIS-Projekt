26fb6958ba8ce58f7fbd543576b9876cfd87b827
Nikolas Nosál <xnosal01@stud.fit.vutbr.cz>
2023-11-22 21:06:52 +0100

    Added front-end for "AssignVehicles"

--- IS_app/app/Livewire/AssignVehiclesListHistory.php
+++ IS_app/app/Livewire/AssignVehiclesListHistory.php
@@ -3,9 +3,65 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class AssignVehiclesListHistory extends Component
 {
+    /* ATRIBUTES */
+
+    /* All data property */
+    public $scheduledRoutes;
+
+    
+    /* FUNCTIONS */
+
+    /* maintenanceGetAll()
+    DESCRIPTION:    - Function which gets all the ScheduledRoutes from the database and formats them
+                    - Returns an array of scheduledRoutes
+
+    TODO:           - Get all scheduledRoutes from the database which are in the past
+                    - Get additional information about the scheduled route
+                    - Format the scheduledRoute data
+    */
+    private function maintenanceGetAll()
+    {
+        // Retrieve all maintenance records from the database
+        $dbScheudledRoutes = [1]; // TODO - get all scheduled routes from the database filer by date (in the past)
+        
+        // Initialize an empty array to store maintenance data
+        $scheduledRoutes = [];
+        
+        // Loop through each maintenance record and format the data
+        foreach ($dbScheudledRoutes as $dbScheudledRoute) {
+
+            // TODO - get additional information about the scheduled route
+
+            // TODO - format the maintenance data 
+            $scheduledRoutes[] = [
+                'id' => "N/A",
+                'link' => "N/A",
+                'name' => "N/A",
+                'start' => "N/A",
+                'repeat' => "N/A",
+                'validUntil' => "N/A",
+                'driver' => "N/A",
+                'vehicle' => "N/A",
+            ];
+        }
+        return $scheduledRoutes;
+    }
+
+
+    /* LIVEWIRE */
+
+    /* - Used for mounting the component, with listener to refresh the the list */
+    #[On('assign-vehicles-list-history')]
+    public function mount() {
+        // Get all maintenances from the database
+        $this->scheduledRoutes = $this->maintenanceGetAll();
+    }
+
+    /* - Used for rendering the component in the browser */
     public function render()
     {
         return view('livewire.assign-vehicles-list-history');
--- IS_app/resources/views/livewire/assign-vehicles-list-history.blade.php
+++ IS_app/resources/views/livewire/assign-vehicles-list-history.blade.php
@@ -1,3 +1,34 @@
 <div>
-    <div>history</div>
+    <!-- Table -->
+    <table class="list-table">
+        <thead class="list-up-body"> <!-- Table Header -->
+            <tr class="list-up-row">
+                <th class="list-up-box">Priradený vodič</th>
+                <th class="list-up-box">Priradené vozidlo</th>
+                <th class="list-up-box">Linka</th>
+                <th class="list-up-box">Názov trasy</th>
+                <th class="list-up-box">Začiatok trasy</th>
+                <th class="list-up-box">Opakovanie</th>
+                <th class="list-up-box">Platné do</th>
+                <th class="list-up-box"></th>
+            </tr>
+        </thead>
+        <tbody class="list_low_body">
+            @forelse ($scheduledRoutes as $scheduledRoute)
+                <tr class="list-low-row">
+                    <td class="list-low-box"> {{ $scheduledRoute['driver'] }} </td>
+                    <td class="list-low-box"> {{ $scheduledRoute['vehicle'] }} </td>
+                    <td class="list-low-box"> {{ $scheduledRoute['link'] }} </td>
+                    <td class="list-low-box"> {{ $scheduledRoute['name'] }} </td>
+                    <td class="list-low-box"> {{ $scheduledRoute['start'] }} </td>
+                    <td class="list-low-box"> {{ $scheduledRoute['repeat'] }} </td>
+                    <td class="list-low-box"> {{ $scheduledRoute['validUntil'] }} </td>
+                </tr>
+            @empty
+            <tr class="list-low-row">   <!-- No Vehicle -->
+                <td class="list-low-box">V databáze nie je žiaden dokončený plánovaný spoj</td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
 </div>
