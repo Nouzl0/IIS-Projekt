d6c22eb9b07f4ff1e3005daddb5f22104fe22748
SamuelZima <samuel020327@gmail.com>
2023-11-26 19:20:02 +0100

    Fixed Timetables in HomeListTimetables

--- IS_app/app/Livewire/HomeListTimetables.php
+++ IS_app/app/Livewire/HomeListTimetables.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Usek;
+use App\Models\Trasa;
 use Illuminate\Support\Facades\DB;
 
 class HomeListTimetables extends Component
@@ -36,44 +37,63 @@ class HomeListTimetables extends Component
     /* LIVEWIRE */
 
     /* mount()
-    DESCRIPTION:    - Component constructor which build the public transport timetable from the database
+    DESCRIPTION:    - Component constructor which builds the public transport timetable from the database
                     - Constructor of the component
-
-    TODO:           - Check if error handling is needed add it or remove my code snippet
     */
     public function mount()
     {
-        // Retrieve all stops(zastavky) from the DB
-        $stops = DB::table('zastavka')->pluck('meno_zastavky', 'id_zastavka')->toArray();
+        // Get all stops from the database
+        $zastavky = DB::table('zastavka')->pluck('meno_zastavky', 'id_zastavka')->toArray();
 
-        // Retrieve all necessary information about lines(linky), routes(trasy) and sections(useky)
-        $results = Usek::select(
-            'usek.*',
-            'trasa.meno_trasy',
-            'linka.cislo_linky',
-            'linka.vozidla_linky',
+        // Get all routes with additional informaton about the line of the route
+        $linky_trasy = Trasa::select(
+            'trasa.*',
+            'linka.cislo_linky'
         )
-        ->join('trasa', 'usek.id_trasa', '=', 'trasa.id_trasa')
         ->join('linka', 'trasa.id_linka', '=', 'linka.id_linka')
         ->get()
         ->toArray();
-        
-        // First loop through the query results, to firstly fill only the cislo_linky into the timetables array
-        foreach ($results as $result) {
-            if (in_array($result['cislo_linky'], $this->timetables) === false) {        // if the cislo_linky is not in the array
-                $this->timetables[$result['cislo_linky']] = [];                         // append it to the timetables array
-            }
+
+        // First loop through the routes, to firstly fill only the cislo_linky into the timetables array
+        foreach ($linky_trasy as $linka_trasa) {
+            $this->timetables[$linka_trasa['cislo_linky']] = [];
         }
 
-        // Second loop through the query results, to fill the zastavky(stops) array of each cislo_linky(line_number)
-        foreach ($results as $result) {
-            if ( (($result['id_trasa'] % 2 != 0) && (array_key_exists($result['id_zastavka_zaciatok'], $stops))) || (($result['id_trasa'] % 2 == 0) && ($result['poradie_useku'] == 1)) ) {     // if the id_trasa is an odd number and the zastavka(stop) exists in DB or the id_trasa is an even number and the poradie_useku is one (complete loop of the route) 
-                $this->timetables[$result['cislo_linky']][] = $stops[$result['id_zastavka_zaciatok']];                  // append the name of the zastavka(stop) to the array of the appropriate cislo_linky(line_number)
-            }
-        }
+        // Variables of previous route and line for checks
+        $predosla_id_trasa = null;
+        $predosla_id_linka = null;
 
-        // TODO - Error handling
-        //'$this->dispatch('alert-error', message: $errorMessage);'
+        // Second loop through the routes, to fill the stops of each line number
+        foreach ($linky_trasy as $linka_trasa) {
+
+            // If the id_trasa changed and the id_linka didn't, skip the current route because we don't want all the stops to be shown two times, but only once
+            if (($predosla_id_trasa !== $linka_trasa['id_trasa']) && ($predosla_id_linka === $linka_trasa['id_linka']) && ($predosla_id_trasa !== null) && ($predosla_id_linka !== null)) {
+                $predosla_id_trasa = $linka_trasa['id_trasa'];
+                $predosla_id_linka = $linka_trasa['id_linka'];
+                continue;
+            }
+
+            // Get all sections for the current route
+            $useky_na_trase = Usek::where('id_trasa', '=', $linka_trasa['id_trasa'])->get()->toArray();
+
+            $pocet_usekov_na_trase = count($useky_na_trase);        // count all the sections on the selected route
+            $pocet_spracovanych_usekov = 0;                         // set a counter for the already handled sections
+
+            // Loop through the sections of the current route and add all the stops for the current route
+            foreach ($useky_na_trase as $usek_na_trase) {
+                $pocet_spracovanych_usekov++;
+
+                $this->timetables[$linka_trasa['cislo_linky']][] = $zastavky[$usek_na_trase['id_zastavka_zaciatok']];
+
+                if ($pocet_spracovanych_usekov === $pocet_usekov_na_trase) {
+                    $this->timetables[$linka_trasa['cislo_linky']][] = $zastavky[$usek_na_trase['id_zastavka_koniec']];
+                }
+            }
+
+            // Set the helper variables for previous route
+            $predosla_id_trasa = $linka_trasa['id_trasa'];
+            $predosla_id_linka = $linka_trasa['id_linka'];
+        }
     }
 
     /* - Used for rendering the component in the browser */
